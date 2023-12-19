2abd1f10cdc587e93feb6e272653328cd7cea256
Valentin Levin <levin_valentin@mail.ru>
2023-12-19 09:30:44 +0600

    solution #1

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,88 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
-
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.logging.Logger;
 
 public class FileReader {
+    private static final Logger logger = Logger.getLogger(FileReader.class.getName());
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        String[] fileData = readDataFromFile(file);
+
+        for (String dataLine : fileData) {
+            KeyValue keyValue = KeyValue.fromString(dataLine);
+            if (keyValue != null) {
+                switch (keyValue.getKey()) {
+                    case "name":
+                        profile.setName(keyValue.getValue());
+                        break;
+                    case "email":
+                        profile.setEmail(keyValue.getValue());
+                        break;
+                    case "phone":
+                        Long phone = Long.parseLong(keyValue.getValue());
+                        profile.setPhone(phone);
+                        break;
+                    case "age":
+                        Integer age = Integer.parseInt(keyValue.getValue());
+                        profile.setAge(age);
+                        break;
+                    default:
+                        logger.info("Unknown parameter: " + keyValue.getKey());
+                }
+            }
+        }
+
+        return profile;
+    }
+
+    private String[] readDataFromFile(File file) {
+        try (
+                RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r");
+                FileChannel channel = randomAccessFile.getChannel()
+        ) {
+            ByteBuffer buffer = ByteBuffer.allocate((int) channel.size());
+            channel.read(buffer);
+            buffer.flip();
+            String fileData = new String(buffer.array());
+            return fileData.split("\\r\\n");
+        } catch (IOException e) {
+            logger.info(e.getMessage());
+            return new String[]{};
+        }
+    }
+}
+
+class KeyValue {
+    private static final char KEY_VALUE_SEPARATOR = ':';
+
+    private String key;
+    private String value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static KeyValue fromString(String value) {
+        int keyValueSeparatorPos = value.indexOf(KEY_VALUE_SEPARATOR);
+        if (keyValueSeparatorPos != -1) {
+            KeyValue keyValue = new KeyValue();
+
+            keyValue.key = value.substring(0, keyValueSeparatorPos).trim().toLowerCase();
+            keyValue.value = value.substring(keyValueSeparatorPos + 1).trim();
+
+            return keyValue;
+        } else {
+            return null;
+        }
     }
 }
